b9ce68a586f6b6841a36f010f62d7556eb3443fb
Matt Karpa <mattkarpa@gmail.com>
2011-04-02 17:12:21 -0700

    Handled exception that arised when a player tried to open a chest with no item in hand

--- src/minemail/MineMailPlayerListener.java
+++ src/minemail/MineMailPlayerListener.java
@@ -18,24 +18,28 @@ public class MineMailPlayerListener extends PlayerListener{
 	public void onPlayerInteract(PlayerInteractEvent event){
 		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
 			if(event.getClickedBlock().getType().equals(Material.CHEST)){
-				if(event.getItem().getType().equals(Material.ARROW))
-				{
-					//Matt- we could probably make this more object-oriented. Maybe a ChestMail class?
-					BlockCoords coords = new BlockCoords(event.getClickedBlock());
-					Configuration config = plugin.getConfiguration();
-					config.load();
-					//Seeing if the chest is already set by accessing it using the coords, if it doesn't return a value then we use the exception catching to register it
-					try {
-						config.getString(coords.getCoords()); //This will return an exception if it can't get the coordinate key
-						event.getPlayer().sendMessage("This chest is already registered.");
-						event.setCancelled(true);
-					} catch(Exception e) {
-						//We'll register the chest through this
-						config.setProperty(coords.getCoords(),event.getPlayer().getDisplayName().toLowerCase());
-						config.save();
-						event.getPlayer().sendMessage("This chest is now registered as your mailbox.");
-						event.setCancelled(true);
+				try{
+					if(event.getItem().getType().equals(Material.ARROW))
+					{
+						//Matt- we could probably make this more object-oriented. Maybe a ChestMail class?
+						BlockCoords coords = new BlockCoords(event.getClickedBlock());
+						Configuration config = plugin.getConfiguration();
+						config.load();
+						//Seeing if the chest is already set by accessing it using the coords, if it doesn't return a value then we use the exception catching to register it
+						try {
+							config.getString(coords.getCoords()); //This will return an exception if it can't get the coordinate key
+							event.getPlayer().sendMessage("This chest is already registered.");
+							event.setCancelled(true);
+						} catch(Exception e) {
+							//We'll register the chest through this
+							config.setProperty(coords.getCoords(),event.getPlayer().getDisplayName().toLowerCase());
+							config.save();
+							event.getPlayer().sendMessage("This chest is now registered as your mailbox.");
+							event.setCancelled(true);
+						}
 					}
+				} catch (Exception e) {
+					//do nothing
 				}
 			}
 			
