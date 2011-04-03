a420223443dd9baab1fdc3022e8b38db5abc548c
Andrew Comminos <andrewcomminos@gmail.com>
2011-04-02 21:36:38 -0700

    Reverse engineered in Visual Basic

---
+++ src/minemail/MailChest.java
@@ -0,0 +1,34 @@
+package minemail;
+
+import org.bukkit.Material;
+import org.bukkit.block.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.util.config.Configuration;
+
+public class MailChest {
+	
+	Block chestblock;
+	Configuration config;
+	BlockCoords coords;
+	
+	public MailChest(Block b, Configuration c) {
+		chestblock = b;
+		config = c;
+		config.load();
+		coords = new BlockCoords(b);
+	}
+	
+	public void setOwner(Player player) {
+		config.setProperty(coords.getCoords(),player.getDisplayName().toLowerCase());
+		config.save();
+	}
+	
+	public String getOwnerName() {
+		String playername = config.getString(coords.getCoords());
+		return playername;
+	}
+
+}
--- src/minemail/MineMailPlayerListener.java
+++ src/minemail/MineMailPlayerListener.java
@@ -2,6 +2,7 @@ package minemail;
 
 import org.bukkit.Material;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -9,6 +10,7 @@ import org.bukkit.util.config.Configuration;
 
 public class MineMailPlayerListener extends PlayerListener{
 	MineMail plugin;
+	Configuration config = plugin.getConfiguration();
 	
 	public MineMailPlayerListener(MineMail mineMail){
 		plugin = mineMail;
@@ -18,18 +20,13 @@ public class MineMailPlayerListener extends PlayerListener{
 	public void onPlayerInteract(PlayerInteractEvent event){
 		if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
 			if(event.getClickedBlock().getType().equals(Material.CHEST)){
-				BlockCoords coords = new BlockCoords(event.getClickedBlock());
-				Configuration config = plugin.getConfiguration();
-				config.load();
+				MailChest chest = new MailChest(event.getClickedBlock(),config);
 				try {
 					if(event.getItem().getType().equals(Material.ARROW))
 					{
-						//Matt- we could probably make this more object-oriented. Maybe a ChestMail class?
-						//Seeing if the chest is already set by accessing it using the coords, if it doesn't return a value then we use the exception catching to register it
-						String configcoords = config.getString(coords.getCoords()); //This will return an exception if it can't get the coordinate key
-						if(configcoords == null) {
-							config.setProperty(coords.getCoords(),event.getPlayer().getDisplayName().toLowerCase());
-							config.save();
+						String playerowner = chest.getOwnerName();
+						if(playerowner == null) {
+							chest.setOwner(event.getPlayer());
 							event.getPlayer().sendMessage("This chest is now registered as your mailbox.");
 							event.setCancelled(true);
 						} else {
@@ -42,7 +39,7 @@ public class MineMailPlayerListener extends PlayerListener{
 					//Do nothing
 				}
 				
-				if(!event.getPlayer().getDisplayName().toLowerCase().equals(config.getString(coords.getCoords()))) {
+				if(!chest.getOwnerName().equals(event.getPlayer().getDisplayName().toLowerCase())) {
 					event.getPlayer().sendMessage("This ain't your mailbox, bro.");
 					event.setCancelled(true);
 				}
